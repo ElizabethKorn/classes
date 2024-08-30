5cfdfd4c48585836b0c80b8cb2fccdc50fded4df
Liza <lkorn036@gmail.com>
2024-08-30 14:30:27 -0500

    one test added

--- src/js/__tests__/character.test.js
+++ src/js/__tests__/character.test.js
@@ -1,5 +1,14 @@
 import Character from "../character";
 
+test("should throw an error for invalid name length", () => {
+  expect(() => new Character("R", "Bowman")).toThrow(
+    "Имя должно быть строкой длиной от 2 до 10 символов"
+  );
+  expect(() => new Character("Rnsdkcndnvknkd", "Bowman")).toThrow(
+    "Имя должно быть строкой длиной от 2 до 10 символов"
+  );
+});
+
 test("should throw an error for invalid character type", () => {
   expect(() => new Character("Robin", "type")).toThrow(
     "Тип должен быть одним из следующих: Bowman, Swordsman, Magician, Daemon, Undead, Zombie"
